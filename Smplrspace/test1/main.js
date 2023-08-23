84f7c7bb8929a00103d95ceabcd39ed1face7c21
Chadi-Hammoud <chadihammoud96@gmail.com>
2023-08-23 16:07:28 +0300

    23-Aug-2023

--- Smplrspace/test1/main.js
+++ Smplrspace/test1/main.js
@@ -1,19 +1,21 @@
 import { loadSmplrJs } from "./node_modules/@smplrspace/smplr-loader";
-import { office } from "./data.js"
+// import { office } from "./data.js"
 
 loadSmplrJs("esm")
     .then((smplr) => {
         const space = new smplr.Space({
-            spaceId: "de022c7b-ba57-428a-8eea-7f1a2189a2ac",
-            clientToken: "pub_fc4b7f5e33bd49cf98912c56c404de8c",
+            // spaceId: "7d9cb58a-2fd1-43ee-9c0f-4254d78ec166",
+            // clientToken: "pub_fc4b7f5e33bd49cf98912c56c404de8c",
+            
+            spaceId:"de022c7b-ba57-428a-8eea-7f1a2189a2ac",
+            clientToken:"pub_fc4b7f5e33bd49cf98912c56c404de8c",            
             containerId: "container",
         });
         space.startViewer({
             preview: false,
             mode: '3d' ,
-            // allowModeChange: false,
             onReady: () => {
-                updateDataLayers();
+                // updateDataLayers();
                 console.log("Viewer is ready");
             },
             onError: (error) => console.error("Could not start viewer", error),
@@ -27,35 +29,35 @@ loadSmplrJs("esm")
 
 
 
-function updateDataLayers() {
-    // remove previous layers if any
-    space.removeDataLayer('office');
+// function updateDataLayers() {
+//     // remove previous layers if any
+//     space.removeDataLayer('office');
 
-    // space.addDataLayer({
-    //     id: "office",
-    //     type: 'furniture',
-    //     data: office,
-    //     color: (d) => (d.available ? '#50b268' : '#f75e56'),
-    // })
+//     // space.addDataLayer({
+//     //     id: "office",
+//     //     type: 'furniture',
+//     //     data: office,
+//     //     color: (d) => (d.available ? '#50b268' : '#f75e56'),
+//     // })
     
-    // space.addDataLayer({
-    //     id: 'desks',
-    //     type: 'furniture',
-    //     data: desks,
-    //     tooltip: (d) => `${d.name} - ${d.available ? 'free' : 'occupied'}`,
-    //     color: (d) => (d.available ? '#50b268' : '#f75e56'),
-    //     alpha: 0.7,
-    //     // height: 2.9,
-    // });
+//     // space.addDataLayer({
+//     //     id: 'desks',
+//     //     type: 'furniture',
+//     //     data: desks,
+//     //     tooltip: (d) => `${d.name} - ${d.available ? 'free' : 'occupied'}`,
+//     //     color: (d) => (d.available ? '#50b268' : '#f75e56'),
+//     //     alpha: 0.7,
+//     //     // height: 2.9,
+//     // });
 
-    space.addDataLayer({
-        id: "office",
-        type: 'polygon',
-        color:(d) => (d.available ? '#50b268' : '#f75e56'),
-        alpha:0.5,
-      })
-
-
+//     space.addDataLayer({
+//         id: "office",
+//         type: 'polygon',
+//         color:(d) => (d.available ? '#50b268' : '#f75e56'),
+//         alpha:0.5,
+//       })
 
 
-}
+
+
+// }
---
+++ Smplrspace/test2/main.js
@@ -0,0 +1,43 @@
+import { loadSmplrJs } from "./node_modules/@smplrspace/smplr-loader";
+import { IT_Room } from "./data.js"
+
+loadSmplrJs("esm")
+    .then((smplr) => {
+        const space = new smplr.Space({
+            spaceId: "7d9cb58a-2fd1-43ee-9c0f-4254d78ec166",
+            clientToken: "pub_fc4b7f5e33bd49cf98912c56c404de8c",
+            containerId: "container",
+        });
+
+        space.startViewer({
+            preview: true,
+            // onReady: () => updateDataLayers(),
+            onError: (error) => {
+                console.log('Could not start viewer', error),
+                console.log("######################################");
+                console.log(smplrClient.checkApiConnection());
+                console.log("######################################");
+
+            }
+        });
+
+
+
+    }).catch((error) => console.error(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

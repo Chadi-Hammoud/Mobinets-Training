c6db81b56eb143890503a656df907c41450cff22
Chadi-Hammoud <chadihammoud96@gmail.com>
2023-08-24 11:58:10 +0300

    V1.1

--- Smplrspace/24-08-2023/data.js
+++ Smplrspace/24-08-2023/data.js
@@ -4,6 +4,8 @@ export const Servers = [
     layerType: "furniture",
     smplrUuid: "85c71216-9869-430b-b985-0195df352546",
     name: "Server1",
+    type: "CISCO",
+    temprature: 75,
     furnitureId: "8f08beb8-2627-44d6-add4-19a380b55912"
   },
   {
@@ -11,6 +13,8 @@ export const Servers = [
     layerType: "furniture",
     smplrUuid: "0014c1d2-8780-428d-bc42-be25d8f01fcb",
     name: "Server2",
+    type: "Dell",
+    temprature: 60,
     furnitureId: "39a8ad5d-ad31-4633-b5b2-86982ad3aae0"
   },
   {
@@ -18,6 +22,8 @@ export const Servers = [
     layerType: "furniture",
     smplrUuid: "0848602c-c2c6-4c31-91ee-0930133fe5db",
     name: "Server3",
+    type: "IBM",
+    temprature: 50,
     furnitureId: "bc7cbee0-c548-4b8a-bdd6-83bc00eda81e"
   },]
 
@@ -26,6 +32,7 @@ export const Desk = [{
   layerType: "furniture",
   smplrUuid: "8e58d216-b0f8-43c3-ba0f-38ccdf51667c",
   name: "Desk1",
+  employee: "AbdAlrahman",
   furnitureId: "b5e3477f-c5b0-4339-9148-1037034f5a78"
 },
 {
@@ -33,6 +40,7 @@ export const Desk = [{
   layerType: "furniture",
   smplrUuid: "3d46931f-97d9-4635-9672-fb41dec7cc09",
   name: "Desk2",
+  employee: "Mohamad",
   furnitureId: "a8026116-1306-4bf9-a940-1112dff3f7a4"
 },]
 
@@ -42,6 +50,7 @@ export const Laptop = [
     layerType: "furniture",
     smplrUuid: "84fc0e42-f5fb-482c-bdad-ce4ae1d83f30",
     name: "Lap1",
+    employee: "Chadi",
     furnitureId: "04082a37-1515-4621-8245-c63cb2f243b1"
   }
 ]

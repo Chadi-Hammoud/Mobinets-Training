28936b4d5beebb2f7482ce2eba4c8268db255e93
Chadi-Hammoud <chadihammoud96@gmail.com>
2023-08-24 15:11:07 +0300

    last commit

--- Smplrspace/24-08-2023/data.js
+++ Smplrspace/24-08-2023/data.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////////////////////////////
 export const Servers = [
   {
     groupName: "Servers",
@@ -27,6 +28,7 @@ export const Servers = [
     furnitureId: "bc7cbee0-c548-4b8a-bdd6-83bc00eda81e"
   },]
 
+////////////////////////////////////////////////////////////////////////
 export const Desk = [{
   groupName: "Desk",
   layerType: "furniture",
@@ -42,8 +44,11 @@ export const Desk = [{
   name: "Desk2",
   employee: "Mohamad",
   furnitureId: "a8026116-1306-4bf9-a940-1112dff3f7a4"
-},]
+},
 
+];
+
+////////////////////////////////////////////////////////////////////////
 export const Laptop = [
   {
     groupName: "Laptop",
@@ -53,4 +58,106 @@ export const Laptop = [
     employee: "Chadi",
     furnitureId: "04082a37-1515-4621-8245-c63cb2f243b1"
   }
+];
+
+
+////////////////////////////////////////////////////////////////////////
+export const Points = [
+  {
+    layerType: "point",
+    smplrUuid: "6706a88a-7c17-472b-a13d-72ca68089f88",
+    name: "Enterence",
+    levelIndex: "0",
+    position: {x:4.4162787343712715,z:-3.9701508090869697,elevation:2.7805892395820493,levelIndex:0},
+    coordinates: ""
+  },
+];
+
+////////////////////////////////////////////////////////////////////////
+export const Rooms = 
+// [
+//   {
+//     groupName: "Room",
+//     layerType: "polygon",
+//     smplrUuid: "fbf24d64-8d27-4817-81ac-53450b6b62e2",
+//     name: "ServerRoom",
+//     levelIndex: "",
+//     position: "",
+//     coordinates: [
+//       {x:2.9824074418198796,z:-6.961352299245758,levelIndex:0},
+//       {x:2.9360130425310524,z:-5.0689359673663965,levelIndex:0},
+//       {x:12.935890648729387,z:-5.045916665429002,levelIndex:0},
+//       {x:13.010551415649926,z:-6.991195200692417,levelIndex:0}
+//     ]
+//   },
+//   {
+//     groupName: "Room",
+//     layerType: "polygon",
+//     smplrUuid: "8786a668-2925-4274-8820-d4e4f2aabc1f",
+//     name: "OfficePoom",
+//     levelIndex: "",
+//     position: "",
+//     coordinates: [
+//       {x:3.023119699632246,z:-3.0751050266603066,levelIndex:0},
+//       {x:8.797315968393033,z:-3.0760284216134983,levelIndex:0},
+//       {x:8.798347043963968,z:-0.9403615008529689,levelIndex:0},
+//       {x:2.972204937136164,z:-0.9570695684114972,levelIndex:0}
+//     ]
+//   },
+//   {
+//     groupName: ""
+//   }
+// ];
+
+[
+  {
+    groupName: "Room",
+    layerType: "polygon",
+    smplrUuid: "fbf24d64-8d27-4817-81ac-53450b6b62e2",
+    name: "ServerRoom",
+    furnitureId: "",
+    levelIndex: "",
+    position: "",
+    coordinates: [{x:2.9824074418198796,z:-6.961352299245758,levelIndex:0},{x:2.9360130425310524,z:-5.0689359673663965,levelIndex:0},{x:12.935890648729387,z:-5.045916665429002,levelIndex:0},{x:13.010551415649926,z:-6.991195200692417,levelIndex:0}]
+  },
+  {
+    groupName: "Room",
+    layerType: "polygon",
+    smplrUuid: "8786a668-2925-4274-8820-d4e4f2aabc1f",
+    name: "OfficePoom",
+    furnitureId: "",
+    levelIndex: "",
+    position: "",
+    coordinates: [{x:3.023119699632246,z:-3.0751050266603066,levelIndex:0},{x:8.797315968393033,z:-3.0760284216134983,levelIndex:0},{x:8.798347043963968,z:-0.9403615008529689,levelIndex:0},{x:2.972204937136164,z:-0.9570695684114972,levelIndex:0}]
+  },
+  {
+    groupName: ""
+  }
 ]
+
+////////////////////////////////////////////////////////////////////////
+export const Polyline = [
+  {
+    groupName: "Polyline",
+    layerType: "polyline",
+    smplrUuid: "20947884-12a1-41b7-b96c-7378d154ca6f",
+    name: "p1",
+    levelIndex: "",
+    position: "",
+    coordinates: [{
+      x: 10.099920489560006,
+      z: -4.951072297576904,
+      elevation: 2.970654143123502,
+      levelIndex: 0
+    },
+    { x: 10.95188743224198, z: -3.7651046618245574, elevation: 0.0020000000949984553, levelIndex: 0 },
+    { x: 9.493629577164377, z: -3.569621768756785, elevation: 0.0020000000949984553, levelIndex: 0 }, 
+    { x: 9.40801607345321, z: -4.675923544196466, elevation: 0.0020000000949949026, levelIndex: 0 },
+    { x: 9.846429479714292, z: -4.956407702872303, elevation: 3.0000000000000018, levelIndex: 0 }]
+  },
+  {
+    groupName: ""
+  }
+]
+
+
